e4f142bab92df9d70a93977e415b6ab84b4e4d3f
Chavez <dev.echavez@gmail.com>
2025-10-25 00:49:08 -0300

    Criação da pagina Home e configurações do projeto

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,45 @@
 import 'package:flutter/material.dart';
+import 'src/ui/pages/home_page.dart';
+
+// Se o projeto não estiver compilando quando clicar no Start Debugging, excute: flutter run -d chrome
 
 void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Agroview',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: const Color(0xFF1E8F4A),
+        colorScheme: const ColorScheme.light(
+          primary: Color(0xFF1E8F4A),
+          secondary: Color(0xFF1976D2),
+          //background: Color(0xFFFFFFFF),
+        ),
+        useMaterial3: true,
+      ),
+      darkTheme: ThemeData(
+        primaryColor: const Color(0xFF1E8F4A),
+        colorScheme: const ColorScheme.dark(
+          primary: Color(0xFF1E8F4A),
+          secondary: Color(0xFF1976D2),
+        ),
+        useMaterial3: true,
+      ),
+      themeMode: ThemeMode.system,
+      home: const HomePage(),
+    );
+  }
+}
+
+
+/*void main() {
   runApp(const MainApp());
 }
 
@@ -10,11 +49,19 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        primaryColor: const Color(0xFF1E8F4A),
+
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.green,
+          accentColor: const Color(0xFF1976D2),
+          backgroundColor: Colors.white,
         ),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      home: const HomePage();
     );
   }
-}
+}*/
