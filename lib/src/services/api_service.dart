4d268eb288294f700fef753630b2674a16567e5c
Chavez <dev.echavez@gmail.com>
2025-10-27 14:12:10 -0300

    Created APK

--- lib/src/services/api_service.dart
+++ lib/src/services/api_service.dart
@@ -6,7 +6,7 @@ import '../models/api_model.dart';
 import '../models/user_model.dart';
 
 class ApiService {
-  static const String baseUrl = 'http://localhost:3055/api';
+  static const String baseUrl = 'https://agroview.orbneil.com/api';
   late Dio dio;
   String? _token;
 
